8e372c88d425c017fe06a4b1912cdcf44c24e7d6
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-12-29 10:45:13 +0100

    feat(controller): add homepage route and template - 06/ Doctrine ORM
    Added a new route for the homepage in HomeController and created the
    corresponding Twig template. This sets up the basic landing page for
    the application.

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,17 +13,54 @@ use Symfony\Component\Routing\Attribute\Route;
 final class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response
-    {
-        return $this->render('recipe/index.html.twig', []);
+    public function index(
+        Request $request,
+        RecipeRepository $repository,
+        // EntityManagerInterface $em
+    ): Response {
+        $recipes = $repository->findWithDurationLowerThan(20);
+
+        // INFO: Accéde au repository via l' EntityManagerInterface
+        // $recipes = $em->getRepository(Recipe::class)->findWithDurationLowerThan(20);
+
+        // INFO: trouver le repository d'une entité
+        // dd($em->getRepository(Recipe::class));
+
+        // INFO: Ajouter une recette
+        // $recipe = new Recipe();
+        // $recipe->setTitle('Barbe à papa')
+        //     ->setSlug('barbe-a-papa')
+        //     ->setContent('La barbe à papa est une confiserie légère et sucrée ...')
+        //     ->setDuration(2)
+        //     ->setCreatedAt(new \DateTimeImmutable())
+        //     ->setUpdatedAt(new \DateTimeImmutable());
+        // $em->persist($recipe);
+
+        // INFO: Supprimer une recette
+        // $em->remove($recipes[0]);
+
+        // INFO: Sauvegarder les changements - Fais les requêtes SQL
+        // $em->flush();
+
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
 
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9\-]+'])]
-    public function show(string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
+        // $recipe = $recipeRepository->findOneBy(['slug' => $slug]);
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', [
+                'id' => $recipe->getId(),
+                'slug' => $recipe->getSlug()
+            ]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id' => $id
+            'recipe' => $recipe
         ]);
 
         // return new JsonResponse([
