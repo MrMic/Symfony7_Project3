49242db574f9a8c4e5f812d5a1bfa08dd70fdf1c
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-01-09 10:46:33 +0100

    feat(UI+UX): Formulaire de contact - PART 1 - 10/

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\ContactDTO;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request): Response
+    {
+        $data = new ContactDTO();
+        $form = $this->createForm(ContactType::class, $data);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // TODO: Envoi du mail
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
